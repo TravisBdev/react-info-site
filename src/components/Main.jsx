b55fe096b92fa9069eaf114c57163f6a03e426a5
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-16 13:00:02 -0500

    finished the nav and main components - gave them simple styles - so far really liking vite

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main goes here</h1>;
+  return (
+    <main className="main">
+      <h1>Fun Facts About React</h1>
+      <ul className="main-list">
+        <li>Released in 2013</li>
+        <li>Origionally created by Jordan Walke</li>
+        <li>Has over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps</li>
+      </ul>
+    </main>
+  );
 }
